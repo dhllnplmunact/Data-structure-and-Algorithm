5f0cfa1f604a4ef5a3f5859aa0b6793ce42b6a22
dhllnplmunact <agarodhiellan_bsit@plmun.edu.ph>
2025-09-10 16:51:56 +0800

    Add files via upload

---
+++ Activity 4 (do while loop).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int i = 1;
+        
+        do {
+            cout << "do while loop: " << i << endl;
+            i++;                 
+        } while (i <= 20);     
+    return 0;
+}
